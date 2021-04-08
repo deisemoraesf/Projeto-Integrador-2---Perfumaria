909e928308488e129b5cbd15c1db900a44a02ee5
Fernanda <fernandaraeli@gmail.com>
2021-04-07 21:21:43 -0300

    tela inserir produto

---
+++ src/main/java/com/devsakatsuki/perfumariapi2/model/Produto.java
@@ -0,0 +1,85 @@
+package com.devsakatsuki.perfumariapi2.model;
+
+
+public class Produto {
+    
+    private int id;
+    private int codigo;
+    private String nome;
+    private double preco;
+    private String categoria;
+    private String marca;
+    private double quantidade;
+    
+    public Produto(){
+    }
+
+    public Produto(int id, int codigo, String nome, double preco, String categoria, String marca, double quantidade) {
+        this.id = id;
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+        this.marca = marca;
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+   
+        
+}
